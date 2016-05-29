8a10cac94f2e8d4bd31284d7b75588116b50e11d
LorantK <kevin.lorant@gmail.com>
2016-05-29 11:06:43 +0200

    Test commit

--- app/src/main/java/com/example/kevin/clemapp/parsing/Parser.java
+++ app/src/main/java/com/example/kevin/clemapp/parsing/Parser.java
@@ -1,8 +1,11 @@
 package com.example.kevin.clemapp.parsing;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.kevin.clemapp.activities.ItemActivity;
 import com.example.kevin.clemapp.models.Item;
 
 import org.jsoup.Jsoup;
@@ -20,12 +23,18 @@ import java.net.URLConnection;
  */
 public class Parser extends AsyncTask<String, Void, String> {
 
+    private Activity activity;
     private String url;
+    private Item i;
 
-    public Parser(String s){
+    public Parser(Activity ac,String s){
         Log.d("Parsing",s);
+        this.activity = ac;
         this.url = "https://www.amazon.fr/gp/product/B01483WYBW/ref=s9_simh_gw_p107_d1_i3?pf_rd_m=A1X6FK5RDHNB96&pf_rd_s=desktop-1&pf_rd_r=F5RETC6DKF2DV67ZBQFS&pf_rd_t=36701&pf_rd_p=863560947&pf_rd_i=desktop";
+    }
 
+    public String getSeller(String url){
+        return null;
     }
 
    /* @Override
@@ -69,11 +78,12 @@ public class Parser extends AsyncTask<String, Void, String> {
        Document doc = null;
        try {
            doc = Jsoup.connect(url).get();
+
            String name = doc.getElementById("productTitle").text();
            String price = doc.getElementById("priceblock_ourprice").text();
            String seller = "Amazon";
-           Item i = new Item(name,price,seller,url);
-           return i.toString();
+           this.i = new Item(name,price,seller,url);
+           return this.i.toString();
        } catch (IOException e) {
            e.printStackTrace();
        }
@@ -82,8 +92,13 @@ public class Parser extends AsyncTask<String, Void, String> {
 
     @Override
     protected void onPostExecute(String result) {
-        Log.d("post","post");
+
         System.out.println(result);
+        Intent intent = new Intent(this.activity, ItemActivity.class);
+        intent.putExtra("name",i.getName());
+        intent.putExtra("price",i.getPrice());
+        intent.putExtra("seller",i.getSeller());
+        activity.startActivity(intent);
     }
 
 }
